732f03a61aafae604953e907a2b17c0cc8db2537
lswaggeringl <54921253+lswaggeringl@users.noreply.github.com>
2025-03-29 01:12:46 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// папка public с публичными файлами
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/edit', (req,res) => {
+  res.send('Для входа в панель управления, введите ключ:');
+});
+
+app.listen(3000, () => console.log('Server alive on 3000 port'));
